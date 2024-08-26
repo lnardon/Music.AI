a4c33c10b6e6d3e7e79d773f278450dd0c495abf
lnardon <lucaslnardon@gmail.com>
2024-08-26 03:25:01 -0300

    Update SongCard component

--- src/components/SongCard/index.tsx
+++ src/components/SongCard/index.tsx
@@ -1,26 +1,62 @@
+import Link from "next/link";
+import { useFavoritesStore } from "@/stores/favorites";
+import { Heart } from "lucide-react";
+
 const SongCard = ({
-  name,
+  id,
+  title,
   artist,
   cover,
-  isFavorite,
+  index = 0,
 }: {
-  name: string;
+  id: string;
+  title: string;
   artist: string;
   cover: string;
-  isFavorite: boolean;
+  files: any[];
+  index?: number;
 }) => {
+  const { favorites, setFavorites } = useFavoritesStore();
+
   return (
-    <div>
-      <div className="flex items-center justify-between bg-white p-4 rounded-lg shadow-md">
-        <div className="flex items-center">
-          <img src={cover} alt={name} className="w-16 h-16 rounded-lg" />
-          <div className="ml-4">
-            <h3 className="font-bold">{name}</h3>
-            <p className="text-gray-500">{artist}</p>
+    <div
+      key={title}
+      className="flex flex-col items-center justify-start bg-[#262626] p-0 rounded-lg shadow-md cursor-pointer opacity-0 slideUp"
+      style={{
+        animationDelay: `${index * 64}ms`,
+      }}
+    >
+      <Link href={`/song/${id}`} className="w-full">
+        <img
+          src={`/assets/images/${cover}`}
+          alt={title}
+          className="w-full rounded-t-lg rounded-b-none mb-2"
+        />
+        <div className="w-full p-4">
+          <h3 className="font-semibold text-lg leading-6 mb-4">{title}</h3>
+
+          <div className="w-full flex items-center justify-between gap-4">
+            <p className="text-sm text-[#666666]">{artist}</p>
+            <div
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                if (favorites.includes(id)) {
+                  setFavorites(favorites.filter((fav: any) => fav !== id));
+                } else {
+                  setFavorites([...favorites, id]);
+                }
+              }}
+            >
+              <Heart
+                size={20}
+                fill={favorites.includes(id) ? "#F8594E" : "transparent"}
+                stroke={favorites.includes(id) ? "#F8594E" : "white"}
+              />
+            </div>
           </div>
-          <input type="checkbox" name="" id="" checked={isFavorite} />
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
